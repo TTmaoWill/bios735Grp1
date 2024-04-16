bba5ccdada4638b491c7b6002c8eaac9742b1efa
Will Tang <tcw20000623@gmail.com>
2024-04-16 00:05:22 -0400

    Added a summary table and PCA

--- phishing/R/basic_random_eda.R
+++ phishing/R/basic_random_eda.R
@@ -1,14 +1,17 @@
-df <- read.csv("source_data/web-page-phishing.csv")
-z <- rep(NA, ncol(df))
-for (i in 1:ncol(df)){
-    z[i] <- sum(df[,i] == 0)/nrow(df)
-}
+library(table1)
+library(tidyverse)
+# Summary of data by outcome
+df <- read.csv("../source_data/web-page-phishing.csv")
+table1(~ . | phishing, data = df)
 
-sum(is.na(df))
-str(df)
-summary(df)
+# PCA
+y <- df$phishing
+x <- df %>% select(-phishing)
+pca <- prcomp(t(x), scale = F)$rotation
 
-z
-colnames(df)
-unique(df$phishing)
-sum(rowSums(df[3:19] == 0) == ncol(df)-3)
+pca_aug <- data.frame(index = 1:nrow(x), PC1 = pca[,1],PC2 = pca[,2], is_phishing = as.factor(y))
+
+ggplot(pca_aug, aes(x=PC1, y=PC2,col=is_phishing)) +
+    geom_point(alpha=0.1,size=1)
+
+# You would see an outlier at left up corner. It is at row 44518 (with a very long url).
